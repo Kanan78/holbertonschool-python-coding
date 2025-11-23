7696852c48ac515f69b8331ba12e6e2ada087a3d
kanan78 <kmusayev27@gmail.com>
2025-11-23 18:01:37 +0000

    commit

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module defines a Square class."""
+
+
+class Square:
+    """This class defines a square by:(based on 0-square.py)"""
+    def __init__(self, size):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
