1d60811d2b59f27b8dc270b3f09d58fa0bb633c5
kanan78 <kmusayev27@gmail.com>
2025-11-23 18:06:21 +0000

    commit

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -5,7 +5,7 @@
 class Square:
     """This class defines a square by:(based on 0-square.py)"""
     def __init__(self, size=0):
-        if not isinstance(x, int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
