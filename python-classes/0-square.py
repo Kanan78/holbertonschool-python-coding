2d20e62783f0f00a514e5f064961c2f76534527a
kanan78 <kmusayev27@gmail.com>
2025-11-23 17:53:21 +0000

    commit

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,6 +2,6 @@
 " ""This module defines a Square class."""
 class Square:
     """This class represents a square shape with a size."""
-    def __init__(self,size)
+    def __init__(self,size):
         self.__size = size
 
